7bdb6ba42657f0c6fd28ca4017f63eb5c749abdb
Kmadhav824 <kmadhav824@gmail.com>
2026-04-17 00:12:27 +0530

    Revert "Bootstraped Insta->Backend Service"
    This reverts commit f34a155fdcb90ae32542e09e43091a62f0fa30a2.

--- backend/dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.get("/users", (req, res) => {
-    res.json({ message: "Hello World" });
-});
-router.post("/register", async (req, res) => {
-});
-export default router;
-//# sourceMappingURL=auth.js.map
--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoutes from "./routes/auth.js";
-const app = express();
-
-app.use("/auth", authRoutes);
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 3000");
-});
